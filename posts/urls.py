17a3e460dc1358cfd8ccd3789a7f57b625605cc0
amirreza <amir.rsf1380@gmail.com>
2024-01-10 01:45:23 +0330

    debug

--- posts/urls.py
+++ posts/urls.py
@@ -10,6 +10,6 @@ app_name = "posts"
 
 urlpatterns = [
     path("", PostListView.as_view(), name="post_list"),
-    path("post/<slug:slug>/", PostDetailView.as_view(), name="post_detail"),
     path("post/create/", PostCreateView.as_view(), name="post_create"),
+    path("post/<slug:slug>/", PostDetailView.as_view(), name="post_detail"),
 ]
